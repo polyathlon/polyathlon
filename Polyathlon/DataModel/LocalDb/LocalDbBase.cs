9f1c38c0f881f47159c77f235929d65a7f64c649
Яковенко Ярослав <robezgames@mail.ru>
2022-07-29 05:33:37 +0300

    - delete dir Model from .csproj - add flurl request for get content in method GetLocalDbTable in class LocalDbBase.cs

--- Polyathlon/DataModel/LocalDb/LocalDbBase.cs
+++ Polyathlon/DataModel/LocalDb/LocalDbBase.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Collections;
 using Newtonsoft.Json.Linq;
+using Flurl.Http;
+using Flurl.Http.Configuration;
 
 namespace Polyathlon.DataModel;
 
@@ -81,10 +83,31 @@ public sealed class LocalDbBase
             {
                 if (!tables.TryGetValue(request, out result))
                 {
-                    string content = @"{'total_rows':4,'offset':0,'rows':[
+                    /*string content = @"{'total_rows':4,'offset':0,'rows':[
                             { 'id':'module:8997d7edcad3eae911a0c9abb100097a','key':'module:8997d7edcad3eae911a0c9abb100097a','value':{ 'rev':'1-52dc66bc4a76166e8348d4b76e2b4b78'},'doc':{ '_id':'module:8997d7edcad3eae911a0c9abb100097a','_rev':'1-52dc66bc4a76166e8348d4b76e2b4b78','name':'Москва', 'shortName': 'М'} },
                             { 'id':'module:8997d7edcad3eae911a0c9abb100097a','key':'module:8997d7edcad3eae911a0c9abb100097a','value':{ 'rev':'1-52dc66bc4a76166e8348d4b76e2b4b78'},'doc':{ '_id':'module:8997d7edcad3eae911a0c9abb100097a','_rev':'1-52dc66bc4a76166e8348d4b76e2b4b78','name':'Рязанская область', 'shortName': 'Ряз. обл.'} }
-                        ]}";
+                        ]}";*/
+                    
+                    // Start: Get content
+                    string content = string.Empty;
+                    IFlurlClientFactory? flurlClientFactory = new PerBaseUrlFlurlClientFactory();
+                    IFlurlClient flurlClient = flurlClientFactory.Get(Settings.Settings.Data.settingsDB.Host);
+                    IFlurlRequest flurlRequest = flurlClient.Request()
+                        .AppendPathSegments("test-requests-regions", "_all_docs")
+                        .SetQueryParam("include_docs", "true")
+                        .WithBasicAuth(Settings.Settings.Data.settingsDB.UserName, Settings.Settings.Data.settingsDB.Password)
+                        .AllowAnyHttpStatus();
+                    try
+                    {
+                        var response = flurlRequest.GetAsync();// JsonAsync();
+                        var responseBody = response.Result.ResponseMessage;
+                        content = responseBody.Content.ReadAsStringAsync().Result;
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Debug.WriteLine(ex.ToString());
+                    }
+                    // End: Get content
 
                     JObject jModules = JObject.Parse(content);
 
