58e7bd4277db45b373dbb05af7ca00321eaefe12
polyathlon <polyatlon.rsu.edu.ru@gmail.com>
2022-08-23 17:12:02 +0300

    All CRUD works

--- Polyathlon/Db/LocalDb/LocalDatabase.cs
+++ Polyathlon/Db/LocalDb/LocalDatabase.cs
@@ -96,7 +96,8 @@ public sealed class LocalDatabase {
     private string CheckEntityContent(Flurl.Url url) {
         string content = string.Empty;
         IFlurlClient flurlClient = flurlClientFactory.Value.Get(url.Root);
-        IFlurlRequest flurlRequest = flurlClient.Request().AppendPathSegments(url.PathSegments)            
+        IFlurlRequest flurlRequest = flurlClient.Request()
+            .AppendPathSegments(url.PathSegments)            
             .WithBasicAuth(Settings.Settings.Data.settingsDB.UserName, Settings.Settings.Data.settingsDB.Password)
             .AllowAnyHttpStatus();
         try {
@@ -121,23 +122,124 @@ public sealed class LocalDatabase {
         IFlurlClient flurlClient = flurlClientFactory.Value.Get(url.Root);
         IFlurlRequest flurlRequest = flurlClient.Request()
             .AppendPathSegments(url.PathSegments)
-            .SetQueryParam(url.Query)
-            .WithBasicAuth(Settings.Settings.Data.settingsDB.UserName, Settings.Settings.Data.settingsDB.Password)
-            .AppendPathSegments(url.PathSegments)
+            //.SetQueryParam(url.Query)
+            .WithBasicAuth(Settings.Settings.Data.settingsDB.UserName, Settings.Settings.Data.settingsDB.Password)            
             .AllowAnyHttpStatus();
         try {
-            var response = flurlRequest.PutJsonAsync(new {
-                name = "Республик Адыгея3",
-                code = 2,
-                shortName = "Рес. Адыгея"
-            }
-            ); 
+            //var response = flurlRequest.PutJsonAsync(new {
+            //    name = "Республик Адыгея3",
+            //    code = 2,
+            //    shortName = "Рес. Адыгея"
+            //}
+            //var response = flurlRequest.PutStringAsync( @"{
+            //    'name': 'Республик Адыгея3',
+            //    'code': '2',
+            //    'shortName': 'Рес. Адыгея'
+            //}"
+            
+            var response = flurlRequest.PostJsonAsync(new {
+                _id = "my:SpaghettiWithMeatballs",
+                name = "1111",
+                age = "2222"
+            }); 
 
             
             //response.Wait();
             var responseBody = response.Result.ResponseMessage;
 
-            //content = responseBody.Content.ReadAsStringAsync().Result;
+            content = responseBody.Content.ReadAsStringAsync().Result;
+            HttpResponseHeaders headers = responseBody.Headers;
+            string result = headers?.ETag?.Tag ?? "";
+            System.Diagnostics.Debug.WriteLine(result);
+            return result;
+        }
+        catch (Exception ex) {
+            System.Diagnostics.Debug.WriteLine(ex.ToString());
+        }
+        return "11";
+    }
+
+    private string ChangeEntityContent(Flurl.Url url) {
+        string content = string.Empty;
+        IFlurlClient flurlClient = flurlClientFactory.Value.Get(url.Root);
+        IFlurlRequest flurlRequest = flurlClient.Request()
+            .AppendPathSegments(url.PathSegments)
+            .SetQueryParam(url.Query)
+            .WithBasicAuth(Settings.Settings.Data.settingsDB.UserName, Settings.Settings.Data.settingsDB.Password)
+            .AllowAnyHttpStatus();
+        try {
+            //var response = flurlRequest.PutJsonAsync(new {
+            //    name = "Республик Адыгея3",
+            //    code = 2,
+            //    shortName = "Рес. Адыгея"
+            //}
+            //var response = flurlRequest.PutStringAsync( @"{
+            //    'name': 'Республик Адыгея3',
+            //    'code': '2',
+            //    'shortName': 'Рес. Адыгея'
+            //}"
+
+            //var response = flurlRequest.PostJsonAsync(new {                
+            //    name = "3333",
+            //    age = "4444"
+            //});
+
+            var response = flurlRequest.PutJsonAsync(new {
+                name = "3333",
+                age = "4444"
+            });
+
+            //response.Wait();
+            var responseBody = response.Result.ResponseMessage;
+
+            content = responseBody.Content.ReadAsStringAsync().Result;
+            HttpResponseHeaders headers = responseBody.Headers;
+            string result = headers?.ETag?.Tag ?? "";
+            System.Diagnostics.Debug.WriteLine(result);
+            return result;
+        }
+        catch (Exception ex) {
+            System.Diagnostics.Debug.WriteLine(ex.ToString());
+        }
+        return "11";
+    }
+
+    private string DeleteEntityContent(Flurl.Url url) {
+        string content = string.Empty;
+        IFlurlClient flurlClient = flurlClientFactory.Value.Get(url.Root);
+        IFlurlRequest flurlRequest = flurlClient.Request()
+            .AppendPathSegments(url.PathSegments)
+            .SetQueryParam(url.Query)
+            .WithBasicAuth(Settings.Settings.Data.settingsDB.UserName, Settings.Settings.Data.settingsDB.Password)
+            .AllowAnyHttpStatus();
+        try {
+            //var response = flurlRequest.PutJsonAsync(new {
+            //    name = "Республик Адыгея3",
+            //    code = 2,
+            //    shortName = "Рес. Адыгея"
+            //}
+            //var response = flurlRequest.PutStringAsync( @"{
+            //    'name': 'Республик Адыгея3',
+            //    'code': '2',
+            //    'shortName': 'Рес. Адыгея'
+            //}"
+
+            //var response = flurlRequest.PostJsonAsync(new {                
+            //    name = "3333",
+            //    age = "4444"
+            //});
+
+            //var response = flurlRequest.PutJsonAsync(new {
+            //    name = "3333",
+            //    age = "4444"
+            //});
+
+            var response = flurlRequest.DeleteAsync();
+
+            //response.Wait();
+            var responseBody = response.Result.ResponseMessage;
+
+            content = responseBody.Content.ReadAsStringAsync().Result;
             HttpResponseHeaders headers = responseBody.Headers;
             string result = headers?.ETag?.Tag ?? "";
             System.Diagnostics.Debug.WriteLine(result);
@@ -178,10 +280,15 @@ public sealed class LocalDatabase {
 
     public void SaveEntity<TEntity>(string request)
     where TEntity : EntityBase {
-        
-        Url url = "http://base.rsu.edu.ru:5984/polyathlon/region:3a1c079241b4d051b71e77e78c024b3a?rev=2-68f931f3384fae155f530fd6275f6077";
 
-        string content = SaveEntityContent(url);
+        //Url url = "http://base.rsu.edu.ru:5984/polyathlon/region:3a1c079241b4d051b71e77e78c024b3a?rev=3-9dc8f4fb36133da152bd1a525af3c4ee";
+        //Url url = "http://base.rsu.edu.ru:5984/polyathlon/my:SpaghettiWithMeatballs?rev=2-b474070511544efa35e9719fe109724f"; //
+        //Url url = "http://base.rsu.edu.ru:5984/polyathlon/my:ant"; //
+
+
+        Url url = "http://base.rsu.edu.ru:5984/polyathlon/my:SpaghettiWithMeatballs?rev=2-b474070511544efa35e9719fe109724f";
+
+        string content = DeleteEntityContent(url);
 
         //JObject jModules = JObject.Parse(content);
         //IList<JToken> rows = jModules["rows"].Children().ToList();
